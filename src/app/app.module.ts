3b7908a4c9de59f4de0dd990373053c90f5d127d
Evgeniy_Ratnikau <evgeniy_ratnikau@epam.com>
2023-05-21 16:17:13 +0300

    feat: add resizable table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ResizableTableComponent } from './resizable-table/resizable-table.component';
+import { ResizeColumnDirective } from './resizable-table/resize-column.derective';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [ResizableTableComponent],
+  declarations: [AppComponent, ResizableTableComponent, ResizeColumnDirective],
+  bootstrap: [AppComponent, ResizableTableComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
